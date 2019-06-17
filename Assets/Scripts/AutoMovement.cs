fcf11dabf25f01875dd54720bb79a11d07a9c0f7
Alpin Türkoglu <alpin.turkoglu@gmail.com>
2019-06-17 22:45:51 +0200

    Walking actor is added

--- Assets/Scripts/AutoMovement.cs
+++ Assets/Scripts/AutoMovement.cs
@@ -1,12 +1,33 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AutoMovement : MonoBehaviour
 {
+    private Vector3 manPosition;
+    public Animator anim;
+    public bool stop;
+
     // Update is called once per frame
+    private void Start()
+    {
+        manPosition = new Vector3(5.98f,-0.34f,-7.92f);
+    }
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime);
+        if(stop)
+        {
+            anim.SetTrigger("isWalking");
+        }
+        else
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * 5);
+            Debug.Log("Man" + manPosition);
+            if (manPosition.x.Equals(gameObject.transform.position.x) && manPosition.y.Equals(gameObject.transform.position.y) && manPosition.z.Equals(gameObject.transform.position.z))
+            {
+                Debug.Log("Hellooooo");
+            }
+        }
     }
 }
